857aa79e0f25981826912ee07fc1afd3911341a9
Chun-Chieh Li <ccli8@nuvoton.com>
2021-03-16 15:46:09 +0800

    Platform: nuvoton/m2354: Enable TRNG entropy source
    1.  Add cmake stuff for enabling crypto accelerator
    2.  Implement mbedtls_hardware_poll using TRNG
    3.  Remove TRNG platform extra secure functions due to replaced with above
    
    Change-Id: I9132d6792cd4ce3770eaa0704b7290835c356bfc

--- platform/ext/target/nuvoton/m2354/config.cmake
+++ platform/ext/target/nuvoton/m2354/config.cmake
@@ -10,3 +10,6 @@ set(PS_MAX_ASSET_SIZE       512    CACHE STRING    "The maximum asset size to be
 set(PS_NUM_ASSETS           12     CACHE STRING    "The maximum number of assets to be stored in the Protected Storage area")
 set(ITS_NUM_ASSETS          12     CACHE STRING    "The maximum number of assets to be stored in the Internal Trusted Storage area")
 set(BL2_TRAILER_SIZE        0x800  CACHE STRING    "Trailer size")
+
+set(CRYPTO_HW_ACCELERATOR               ON          CACHE BOOL      "Whether to enable the crypto hardware accelerator on supported platforms")
+set(TFM_MBEDCRYPTO_PLATFORM_EXTRA_CONFIG_PATH   ${CMAKE_CURRENT_SOURCE_DIR}/platform/ext/target/nuvoton/m2354/accelerator/mbedtls_accelerator_config.h  CACHE PATH "Config to append to standard Mbed Crypto config, used by platforms to cnfigure feature support")
