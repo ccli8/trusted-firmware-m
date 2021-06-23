21ad7aed9544449c70fa7131fa7b4559de772110
Chun-Chieh Li <ccli8@nuvoton.com>
2021-06-23 17:16:10 +0800

    Platform: nuvoton/m2354: Adjust config in favor of Pelion
    1. Adjust ITS/PS. Enlarge PS especially.
    2. Enlarge mbedtls dedicated heap to avoid mbedtls_calloc() failure in ECDSA.
    3. Enlarge secure RAM size for above to fit
    
    NOTE1: Meet HardFault in below test:
    Running Test Suite PS rollback protection tests (TFM_PS_TEST_4XXX)...
    > Executing 'TFM_PS_TEST_4001'
      Description: 'Check PS area version when NV counters 1/2/3 have the same value'
    Result from stack overflow with larger PS_NUM_ASSETS. Try enlarging stack size in partition manifest:
    tf-m-tests/test/test_services/tfm_ps_test_service/tfm_ps_test_service.yaml
    
    NOTE2: Partition RAM in 16KiB unit
    
    Change-Id: Ibe82a857f3aa7dc45eb69c70369846a9c5fca9ba

--- platform/ext/target/nuvoton/m2354/config_tfm_target.h
+++ platform/ext/target/nuvoton/m2354/config_tfm_target.h
@@ -8,17 +8,45 @@
 #ifndef __CONFIG_TFM_TARGET_H__
 #define __CONFIG_TFM_TARGET_H__
 
-/* Size of output buffer in platform service. */
+/* The maximum asset size to be stored in the Internal Trust Storage area. */
+#if ITS_MAX_ASSET_SIZE != 512
+#pragma message("ITS_MAX_ASSET_SIZE is redefined to 512.")
+#undef ITS_MAX_ASSET_SIZE
+#endif
+#define ITS_MAX_ASSET_SIZE      512
+
+/* The maximum number of assets to be stored in the Internal Trust Storage area. */
+#if ITS_NUM_ASSETS != 15
+#pragma message("ITS_NUM_ASSETS is redefined to 15.")
 #undef ITS_NUM_ASSETS
-#define ITS_NUM_ASSETS       12
+#endif
+#define ITS_NUM_ASSETS          15
 
 /* The maximum asset size to be stored in the Protected Storage area. */
+#if PS_MAX_ASSET_SIZE != 1536
+#pragma message("PS_MAX_ASSET_SIZE is redefined to 1536.")
 #undef PS_MAX_ASSET_SIZE
-#define PS_MAX_ASSET_SIZE    512
+#endif
+#define PS_MAX_ASSET_SIZE       1536
 
 /* The maximum number of assets to be stored in the Protected Storage area. */
+#if PS_NUM_ASSETS != 45
+#pragma message("PS_NUM_ASSETS is redefined to 45.")
 #undef PS_NUM_ASSETS
-#define PS_NUM_ASSETS        12
+#endif
+#define PS_NUM_ASSETS           45
+
+/* Enlarge dedicated heap more for mbedtls_calloc()
+ *
+ * Meet PSA_ERROR_INSUFFICIENT_MEMORY/MBEDTLS_ERR_MPI_ALLOC_FAILED error in mbedtls RSA/ECDSA.
+ * Check mbedtls_memory_buffer_alloc_init() in below:
+ * trusted-firmware-m/secure_fw/partitions/crypto/crypto_init.c
+ */
+#if CRYPTO_ENGINE_BUF_SIZE != 0x2800
+#pragma message("CRYPTO_ENGINE_BUF_SIZE is redefined to 0x2800.")
+#undef CRYPTO_ENGINE_BUF_SIZE
+#endif
+#define CRYPTO_ENGINE_BUF_SIZE  0x2800
 
 /* Without H/W entropy like TRNG, use stored NV seed to provide entropy */
 #if CRYPTO_HW_ACCELERATOR
