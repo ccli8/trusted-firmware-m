ed671c7462a7fb04474a8780d17cd579737502f4
Chun-Chieh Li <ccli8@nuvoton.com>
2021-07-01 17:13:45 +0800

    Platform: nuvoton/m2354: Support SDH as CMSIS Flash driver
    Implementation notes:
    1.  Add cmake variable NU_SDH_CMSIS_FLASH to determine enable or not. Default is ON.
    2.  SDH can only be secure for access to secure DMA buffer.
    3.  Continuing above, NSPE cannot access the secure SDH.
    4.  On enabled, tweak BSP SDH driver:
        (1) Go non-interrupt (NU_SDH_DIS_INTR). Secure interrupt isn't trivial and needs change to partition manifest file.
        (2) Enable response in/data in timeout (NU_SDH_ENA_TIMEOUT). This is to escape trap with defected SD card.
    5.  On enabled, cover upper layer missing doing initializing SDH for read/program/erase operations.
    6.  On enabled, simulate dummy SD card on probing no SD card for MCUboot because MCUboot will panic on read failure.
    7.  On enabled, emulate flash program attribute: Transition to 1 from 0 won't occur.
    8.  With DMA buffer for intermediate, read/program are not limited to sector-aligned.
    
    Change-Id: Iad4cec4e71c37622b688594becf178124e7847eb

--- platform/ext/target/nuvoton/m2354/config.cmake
+++ platform/ext/target/nuvoton/m2354/config.cmake
@@ -16,3 +16,4 @@ set(TFM_MBEDCRYPTO_PLATFORM_EXTRA_CONFIG_PATH   ${CMAKE_CURRENT_SOURCE_DIR}/plat
 set(PLATFORM_DUMMY_NV_SEED              FALSE       CACHE BOOL      "Use dummy NV seed implementation. Should not be used in production.")
 
 set(NU_HXT_PRESENT          OFF     CACHE BOOL      "Whether high-speed external crystal oscillator HXT is present")
+set(NU_SDH_CMSIS_FLASH      ON      CACHE BOOL      "Whether support SDH as CMSIS Flash driver")
