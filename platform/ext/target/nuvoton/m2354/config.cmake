4f4f670de120385055d92d8e0997be821dcd20c8
Chun-Chieh Li <ccli8@nuvoton.com>
2021-07-06 15:16:43 +0800

    Platform: nuvoton/m2354: Support PSA Firmware Update
    1.  Remove boot_hal.c from platform_s, which is necessary only for platform_bl2.
    2.  Support SDH as update stage, which is enabled by default.
        NOTE: For platform_bl2, simulate dummy SD card on detached because MCUboot will panic on read failure.
        NOTE: For platform_s, cover upper layer missing doing SDH initialization.
    3.  Support embedded Flash as update stage, which isn't enabled by default.
    4.  Support multiple image boot, which becomes the default.
    5.  Fix FMC_SECURE_ROM_SIZE (partition_M2354.h)
        Original value becomes incorrect when enabling embedded Flash as update stage
    
    Change-Id: Ia6708a1cb15fe3c45363276866bc32ecda0c3adb

--- platform/ext/target/nuvoton/m2354/config.cmake
+++ platform/ext/target/nuvoton/m2354/config.cmake
@@ -7,7 +7,7 @@
 #
 #-------------------------------------------------------------------------------
 
-set(MCUBOOT_IMAGE_NUMBER    1      CACHE STRING    "Whether to combine S and NS into either 1 image, or sign each separately")
+set(MCUBOOT_IMAGE_NUMBER    2      CACHE STRING    "Whether to combine S and NS into either 1 image, or sign each separately")
 set(BL2_TRAILER_SIZE        0x800  CACHE STRING    "Trailer size")
 
 # Platform-specific configurations
@@ -26,5 +26,11 @@ set(CRYPTO_HW_ACCELERATOR               ON          CACHE BOOL      "Whether to 
 set(TFM_MBEDCRYPTO_PLATFORM_EXTRA_CONFIG_PATH   ${CMAKE_CURRENT_SOURCE_DIR}/platform/ext/target/nuvoton/m2354/accelerator/mbedtls_accelerator_config.h  CACHE PATH "Config to append to standard Mbed Crypto config, used by platforms to cnfigure feature support")
 set(CRYPTO_NV_SEED                      OFF         CACHE BOOL      "Use stored NV seed to provide entropy")
 
+# PSA Firmware Update
+set(PLATFORM_HAS_FIRMWARE_UPDATE_SUPPORT  ON        CACHE BOOL      "Platform supports firmware update, such as network connectivities and bootloader support")
+set(TFM_PARTITION_FIRMWARE_UPDATE       ON          CACHE BOOL      "Enable firmware update partition")
+set(MCUBOOT_DATA_SHARING                ON          CACHE BOOL      "Add sharing of application specific data using the same shared data area as for the measured boot")
+
 set(NU_HXT_PRESENT          OFF     CACHE BOOL      "Whether high-speed external crystal oscillator HXT is present")
-set(NU_SDH_CMSIS_FLASH      ON      CACHE BOOL      "Whether support SDH as CMSIS Flash driver")
+set(NU_UPDATE_STAGE_SDH     ON      CACHE BOOL      "Whether enable SDH as update staging area")
+set(NU_UPDATE_STAGE_FLASH   OFF     CACHE BOOL      "Whether enable embedded flash as update staging area")
