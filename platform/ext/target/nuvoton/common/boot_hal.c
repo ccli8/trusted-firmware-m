6a06fa2c8cca398e6f6aa4e7b3bd72a855616f56
Chun-Chieh Li <ccli8@nuvoton.com>
2021-07-06 15:16:43 +0800

    Platform: nuvoton/m2354: Support PSA Firmware Update
    1.  Remove boot_hal.c from platform_s, which is necessary only for platform_bl2.
    2.  Support SDH as update stage, which is enabled by default.
        NOTE: For platform_bl2, simulate dummy SD card on detached because MCUboot will panic on read failure.
        NOTE: For platform_s, cover upper layer missing doing SDH initialization.
    3.  Support embedded Flash as update stage, which isn't enabled by default.
    4.  Support multiple image boot, which becomes the default.
    5.  Fix FMC_SECURE_ROM_SIZE (partition_M2354.h)
        Original value becomes incorrect when enabling embedded Flash as update stage
    
    Change-Id: Ia6708a1cb15fe3c45363276866bc32ecda0c3adb

--- platform/ext/target/nuvoton/common/boot_hal.c
+++ platform/ext/target/nuvoton/common/boot_hal.c
@@ -7,6 +7,17 @@
 
 #include "cmsis.h"
 #include "region.h"
+#include "flash_map/flash_map.h"
+#include "flash_map_backend/flash_map_backend.h"
+#include "bootutil/bootutil_log.h"
+#include "boot_hal.h"
+#include "Driver_Flash.h"
+
+/* Flash device name must be specified by target */
+extern ARM_DRIVER_FLASH FLASH_DEV_NAME;
+#if NU_UPDATE_STAGE_SDH
+extern ARM_DRIVER_FLASH SDH_FLASH_DEV_NAME;
+#endif
 
 REGION_DECLARE(Image$$, ER_DATA, $$Base)[];
 REGION_DECLARE(Image$$, ARM_LIB_HEAP, $$ZI$$Limit)[];
@@ -31,4 +42,46 @@ __attribute__((naked)) void boot_clear_bl2_ram_area(void)
           "r" (REGION_NAME(Image$$, ARM_LIB_HEAP, $$ZI$$Limit))
         : "r0", "memory"
     );
+}
+
+/* bootloader platform-specific hw initialization */
+int32_t boot_platform_init(void)
+{
+    int32_t result;
+
+    result = FLASH_DEV_NAME.Initialize(NULL);
+    if (result != ARM_DRIVER_OK) {
+        return 1;
+    }
+
+#if NU_UPDATE_STAGE_SDH
+    result = SDH_FLASH_DEV_NAME.Initialize(NULL);
+    if (result != ARM_DRIVER_OK) {
+        return 1;
+    }
+#endif
+
+    return 0;
+}
+
+int flash_device_base(uint8_t fd_id, uintptr_t *ret)
+{
+    switch (fd_id) {
+    case FLASH_DEVICE_ID :
+        *ret = FLASH_DEVICE_BASE;
+        break;
+
+#if NU_UPDATE_STAGE_SDH
+    case SDH_FLASH_DEVICE_ID:
+        *ret = SDH_FLASH_BASE_ADDRESS;
+        break;
+#endif
+
+    default:
+        BOOT_LOG_ERR("invalid flash ID %d", fd_id);
+        *ret = -1;
+        return -1;
+    }
+
+    return 0;
 }
