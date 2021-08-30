44189a209a84728f2ad62854488063729ed7c5ab
Chun-Chieh Li <ccli8@nuvoton.com>
2021-08-30 09:36:20 +0800

    Platform: nuvoton/m2354: Fix TFM_SP_PS_TEST test broken on PS_NUM_ASSETS enlarged
    Refer to its bug report:
    https://developer.trustedfirmware.org/T956
    
    Change-Id: I37df0964dd3d019ad65ed4e455dc4ff2baaef2e8

--- platform/ext/target/nuvoton/m2354/config.cmake
+++ platform/ext/target/nuvoton/m2354/config.cmake
@@ -6,11 +6,20 @@
 #-------------------------------------------------------------------------------
 
 set(MCUBOOT_IMAGE_NUMBER    2      CACHE STRING    "Whether to combine S and NS into either 1 image, or sign each separately")
+
+# TFM_SP_PS_TEST test will break when PS_NUM_ASSETS is enlarged to some value.
+# https://developer.trustedfirmware.org/T956
+if (${TEST_S} OR ${TEST_NS})
+set(PS_MAX_ASSET_SIZE       1536   CACHE STRING    "The maximum asset size to be stored in the Protected Storage area")
+set(PS_NUM_ASSETS           10     CACHE STRING    "The maximum number of assets to be stored in the Protected Storage area")
+set(ITS_MAX_ASSET_SIZE      512    CACHE STRING    "The maximum asset size to be stored in the Internal Trusted Storage area")
+set(ITS_NUM_ASSETS          15     CACHE STRING    "The maximum number of assets to be stored in the Internal Trusted Storage area")
+else()
 set(PS_MAX_ASSET_SIZE       1536   CACHE STRING    "The maximum asset size to be stored in the Protected Storage area")
 set(PS_NUM_ASSETS           45     CACHE STRING    "The maximum number of assets to be stored in the Protected Storage area")
 set(ITS_MAX_ASSET_SIZE      512    CACHE STRING    "The maximum asset size to be stored in the Internal Trusted Storage area")
 set(ITS_NUM_ASSETS          15     CACHE STRING    "The maximum number of assets to be stored in the Internal Trusted Storage area")
-
+endif()
 
 set(CRYPTO_HW_ACCELERATOR               ON          CACHE BOOL      "Whether to enable the crypto hardware accelerator on supported platforms")
 set(TFM_MBEDCRYPTO_PLATFORM_EXTRA_CONFIG_PATH   ${CMAKE_CURRENT_SOURCE_DIR}/platform/ext/target/nuvoton/m2354/accelerator/mbedtls_accelerator_config.h  CACHE PATH "Config to append to standard Mbed Crypto config, used by platforms to cnfigure feature support")
