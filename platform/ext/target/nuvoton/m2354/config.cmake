40e74d6f7199157ad8e40368834b6c437635ecc1
Chun-Chieh Li <ccli8@nuvoton.com>
2021-09-10 14:07:30 +0800

    Platform: nuvoton/m2354: Adjust configuration to fit AWS IoT
    1.  Enlarge ITS max asset number/size
        NOTE: RSA key size is larger
    2.  Enlarge mbedtls dedicated heap
        NOTE: RSA algorithm needs more memory.
        NOTE: psa_aead_decrypt() (for mbedtls_ssl_read()) needs memory proportional to data size.
    
    Change-Id: Ia5dbea50635cd76e94d2b9094c19f51f568fa1be

--- platform/ext/target/nuvoton/m2354/config.cmake
+++ platform/ext/target/nuvoton/m2354/config.cmake
@@ -18,8 +18,8 @@ set(BL2_TRAILER_SIZE        0x800  CACHE STRING    "Trailer size")
 else()
 set(PS_MAX_ASSET_SIZE       1536   CACHE STRING    "The maximum asset size to be stored in the Protected Storage area")
 set(PS_NUM_ASSETS           45     CACHE STRING    "The maximum number of assets to be stored in the Protected Storage area")
-set(ITS_MAX_ASSET_SIZE      512    CACHE STRING    "The maximum asset size to be stored in the Internal Trusted Storage area")
-set(ITS_NUM_ASSETS          15     CACHE STRING    "The maximum number of assets to be stored in the Internal Trusted Storage area")
+set(ITS_MAX_ASSET_SIZE      1536   CACHE STRING    "The maximum asset size to be stored in the Internal Trusted Storage area")
+set(ITS_NUM_ASSETS          30     CACHE STRING    "The maximum number of assets to be stored in the Internal Trusted Storage area")
 set(BL2_TRAILER_SIZE        0x800  CACHE STRING    "Trailer size")
 endif()
 
@@ -27,10 +27,10 @@ set(CRYPTO_HW_ACCELERATOR               ON          CACHE BOOL      "Whether to 
 set(TFM_MBEDCRYPTO_PLATFORM_EXTRA_CONFIG_PATH   ${CMAKE_CURRENT_SOURCE_DIR}/platform/ext/target/nuvoton/m2354/accelerator/mbedtls_accelerator_config.h  CACHE PATH "Config to append to standard Mbed Crypto config, used by platforms to cnfigure feature support")
 set(CRYPTO_NV_SEED                      OFF         CACHE BOOL      "Use stored NV seed to provide entropy")
 
-# Meet PSA_ERROR_INSUFFICIENT_MEMORY/MBEDTLS_ERR_MPI_ALLOC_FAILED error in mbedtls ECDSA. Enlarge dedicated heap more for mbedtls_calloc().
+# Meet PSA_ERROR_INSUFFICIENT_MEMORY/MBEDTLS_ERR_MPI_ALLOC_FAILED error in mbedtls RSA/ECDSA. Enlarge dedicated heap more for mbedtls_calloc().
 # Check mbedtls_memory_buffer_alloc_init() in below:
 # trusted-firmware-m/secure_fw/partitions/crypto/crypto_init.c
-set(CRYPTO_ENGINE_BUF_SIZE              0x2800      CACHE STRING    "Heap size for the crypto backend")
+set(CRYPTO_ENGINE_BUF_SIZE              0x4000      CACHE STRING    "Heap size for the crypto backend")
 
 # PSA Firmware Update
 set(TFM_PARTITION_FIRMWARE_UPDATE       ON          CACHE BOOL      "Enable firmware update partition")
